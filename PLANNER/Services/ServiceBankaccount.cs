edb055b28239f34c5d469b82c8b0feac99ceb7d9
Артем Кунченко <kunchenko911@ukr.net>
2024-08-13 23:10:38 +0300

    Rewrite class ServiceBankaccount

--- PLANNER/Services/ServiceBankaccount.cs
+++ PLANNER/Services/ServiceBankaccount.cs
@@ -1,46 +1,78 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PLANNER.Models
 {
-    public class ServiceBankaccount
+    public static class ServiceBankaccount
     {
-        private readonly AppDbContext _context;
-        public ServiceBankaccount(AppDbContext context)
-        {
-            _context = context;
-        }
-        public void CreateBankaccount(Bankaccount bankaccount)
-        {
-            _context.Bankaccounts.Add(bankaccount);
-            _context.SaveChanges();
+        private static readonly DbContextOptions<AppDbContext> _options;
+        private static readonly IConfiguration _config;
 
-        }
-        public void UpdateBankaccount(Bankaccount bankaccount)
+        static ServiceBankaccount()
         {
-            _context.Bankaccounts.Update(bankaccount);
-            _context.SaveChanges();
+            _config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory() + @"\")
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            string connectionString = _config.GetConnectionString("DefaultConnection");
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            optionsBuilder.UseSqlServer(connectionString);
+
+            _options = optionsBuilder.Options;
         }
-        public void DeleteBankaccount(int id)
+
+        public static void CreateBankaccount(Bankaccount bankaccount)
         {
-            var bankaccount = _context.Bankaccounts.Find(id);
-            if (bankaccount != null)
+            using (var context = new AppDbContext(_options, _config))
             {
-                _context.Bankaccounts.Remove(bankaccount);
-                _context.SaveChanges();
+                context.Bankaccounts.Add(bankaccount);
+                context.SaveChanges();
             }
         }
-        public List<Bankaccount> GetBankaccounts()
+
+        public static void UpdateBankaccount(Bankaccount bankaccount)
         {
-            return _context.Bankaccounts.ToList();
+            using (var context = new AppDbContext(_options, _config))
+            {
+                context.Bankaccounts.Update(bankaccount);
+                context.SaveChanges();
+            }
         }
-        public Bankaccount GetBankaccount(int id)
+
+        public static void DeleteBankaccount(int id)
         {
-            var bankaccount = _context.Bankaccounts.Find(id);
-            return bankaccount;
+            using (var context = new AppDbContext(_options, _config))
+            {
+                var bankaccount = context.Bankaccounts.Find(id);
+                if (bankaccount != null)
+                {
+                    context.Bankaccounts.Remove(bankaccount);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public static List<Bankaccount> GetBankaccounts()
+        {
+            using (var context = new AppDbContext(_options, _config))
+            {
+                return context.Bankaccounts.ToList();
+            }
+        }
+
+        public static Bankaccount GetBankaccount(int id)
+        {
+            using (var context = new AppDbContext(_options, _config))
+            {
+                return context.Bankaccounts.Find(id);
+            }
         }
     }
 }
