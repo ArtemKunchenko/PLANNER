8ac1ee189eef70a65de49848da0d39c505adff7c
Dan17042006 <daniilmam17042006@gmail.com>
2024-08-22 13:41:20 +0200

    Page2 v AddTransaction

---
+++ PLANNER/Views/AddTransaction.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TransactionApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            string type = (TypeComboBox.SelectedItem as ComboBoxItem)?.Content.ToString();
+            string category = CategoryComboBox.Text;
+            string date = DatePicker.SelectedDate?.ToString("dd-MM-yyyy");
+
+            string note = NoteTextBox.Text;
+
+
+        }
+        private void AmountTextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+    }
+}
