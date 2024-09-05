495d4d60a011bd272f6702bb850ce0489962b062
Артем Кунченко <kunchenko911@ukr.net>
2024-09-05 20:13:54 +0300

    Fixed correct showing data in tabe from TransactionListViewModel

---
+++ PLANNER/Models/ListTransactionForTablecs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PLANNER.Models
+{
+    public class ListTransactionForTablecs
+    {
+        public decimal amount { get; set; }
+
+        public string ?incomeOrExpence {  get; set; }
+        public string note { get; set; }
+    
+        public DateTime transaktion_date { get; set; }
+      
+        public Category Category { get; set; }
+
+    }
+}
--- PLANNER/ViewModels/TransactionListViewModel.cs
+++ PLANNER/ViewModels/TransactionListViewModel.cs
@@ -52,6 +52,12 @@ namespace PLANNER.ViewModels
             get => _filteredTransactions;
             set => Set(ref _filteredTransactions, value);
         }
+        private ObservableCollection<ListTransactionForTablecs> _listTransaktionForTable;
+        public ObservableCollection<ListTransactionForTablecs> ListTransaktionForTable
+        {
+            get => _listTransaktionForTable;
+            set => Set(ref _listTransaktionForTable, value);
+        }
 
         public ICommand ApplyFiltersCommand { get; }
         public ICommand ApplyDateFilterCommand { get; }
@@ -74,15 +80,16 @@ namespace PLANNER.ViewModels
         private void LoadInitialTransactions()
         {
             var currentYear = DateTime.Now.Year;
-            var transactions = ServiceTransaktion.GetTransaktions()
+            var transactions = ServiceTransaktion.GetTransaktionsWithCategories()
                 .Where(t => t.transaktion_date.Year == currentYear)
                 .OrderByDescending(t => t.transaktion_date);
             FilteredTransactions = new ObservableCollection<Transaktion>(transactions);
+            SetDataForTable(FilteredTransactions);
         }
 
         private void ApplyFilters()
         {
-            var transactions = ServiceTransaktion.GetTransaktions().AsQueryable();
+            var transactions = ServiceTransaktion.GetTransaktionsWithCategories().AsQueryable();
 
             if (!string.IsNullOrEmpty(SelectedType))
             {
@@ -101,14 +108,16 @@ namespace PLANNER.ViewModels
             }
 
             FilteredTransactions = new ObservableCollection<Transaktion>(transactions.OrderByDescending(t => t.transaktion_date));
+            SetDataForTable(FilteredTransactions);
         }
 
         private void ApplyDateFilter()
         {
-            var transactions = ServiceTransaktion.GetTransaktions()
+            var transactions = ServiceTransaktion.GetTransaktionsWithCategories()
                 .Where(t => t.transaktion_date.Date == SelectedDate.Date)
                 .OrderByDescending(t => t.transaktion_date);
             FilteredTransactions = new ObservableCollection<Transaktion>(transactions);
+            SetDataForTable(FilteredTransactions);
         }
 
         private void ClearFilters()
@@ -119,6 +128,19 @@ namespace PLANNER.ViewModels
             SelectedDate = DateTime.Now;
 
             LoadInitialTransactions();
+        }
+
+        private void SetDataForTable(ObservableCollection<Transaktion> filteredTransaktions)
+        {
+            ListTransaktionForTable = new ObservableCollection<ListTransactionForTablecs>();
+            foreach (var t in filteredTransaktions)
+            {
+                string type=String.Empty;
+                if (t.amount > 0) type = "Income";
+                else type = "Expense";
+                ListTransaktionForTable.Add(new ListTransactionForTablecs { amount = t.amount, incomeOrExpence=type, Category=t.Category, note=t.note, transaktion_date=t.transaktion_date });
+
+            }
         }
     }
 }
